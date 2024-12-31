15bb801c875febb16d243d0f5b600963ccae1806
Didi <yedidic@gmail.com>
2024-12-31 17:36:56 +0200

    fix: add cors to server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import bodyParser from 'body-parser';
+import cors from 'cors'; // Import the cors package
 import * as dotenv from 'dotenv';
 import express from 'express';
 import jwt from 'jsonwebtoken';
@@ -14,13 +15,24 @@ const SECRET_KEY = process.env.SECRET_KEY || 'SECRET_KEY';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Enable CORS
+const corsOptions = {
+  origin: '*', // Replace '*' with specific origins or an array of origins if needed
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Authorization,Content-Type',
+};
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
 
 app.post('/token', (req, res) => {
-  const token = jwt.sign({ user: 'testUser' }, SECRET_KEY, {
-    expiresIn: '7d',
-  });
-  res.send(token);
+  const user: string = req.body.user;
+  if (user === 'cars') {
+    const token = jwt.sign({ user: 'cars' }, SECRET_KEY, {
+      expiresIn: '7d',
+    });
+    res.json({ token });
+  }
 });
 
 //Protected Routes
